14dab4ac5e3220777ee7394177245408ea4e7740
JihoonChung <cjeff000@gmail.com>
2024-06-04 17:58:02 -0400

    updated data collection py

--- ultra_sonic_sensor/Improved_usability/data_collection_v3.1/README/US_data_collection_sequence_run/US_data_collection_sequence_run.ino
+++ ultra_sonic_sensor/Improved_usability/data_collection_v3.1/README/US_data_collection_sequence_run/US_data_collection_sequence_run.ino
@@ -2,12 +2,12 @@
 
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     9  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 100 // Maximum distance we want to ping for (in centimeters).
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters).
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 const int numSamples = 200;
-unsigned long samples[numSamples];
+unsigned int samples[numSamples];
 unsigned long sampleTimes[numSamples];
 unsigned long delayBetweenPings = 16800; // Delay between pings in microseconds
 bool delayInMicroseconds = true; // Default delay is in microseconds
@@ -23,6 +23,8 @@ void loop() {
       collectSamples();
     } else if (command == 'M' || command == 'U') {
       setDelay(command);
+    } else if (command == 'S') {
+      runSequence();
     }
   }
 }
@@ -34,11 +36,9 @@ void collectSamples() {
   for (int i = 0; i < numSamples; i++) {
     unsigned long startTime = micros();
     unsigned long uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
-    
 
     samples[i] = uS;
-    
-    
+
     if (delayInMicroseconds) {
       delayMicroseconds(delayBetweenPings); // Delay in microseconds
     } else {
@@ -89,4 +89,44 @@ void setDelay(char unit) {
   Serial.print(delayBetweenPings);
   Serial.print(" ");
   Serial.println(unit == 'M' ? "milliseconds" : "microseconds");
+}
+
+void runSequence() {
+  for (unsigned long delayValue = 400; delayValue <= 1000; delayValue += 50) {
+    Serial.print("Running sequence with delay: ");
+    Serial.print(delayValue);
+    Serial.println(" microseconds");
+
+    // Reset sampleTimes array
+    memset(sampleTimes, 0, sizeof(sampleTimes));
+
+    for (int i = 0; i < 100; i++) {
+      unsigned long startTime = micros();
+      unsigned long uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+
+      samples[i] = uS;
+
+      delayMicroseconds(delayValue); // Delay in microseconds
+      unsigned long endTime = micros();
+      unsigned long pingDuration = endTime - startTime;
+      sampleTimes[i] = pingDuration;
+    }
+
+    // Print samples for this delay
+    for (int i = 0; i < 100; i++) {
+      Serial.print(i);
+      Serial.print(",");
+      Serial.print(sampleTimes[i]);
+      Serial.print(",");
+      Serial.print(samples[i] / US_ROUNDTRIP_CM);
+      Serial.print(",");
+      Serial.print(samples[i]);
+      Serial.print(",");
+      Serial.print(delayValue);
+      Serial.println(" us");
+    }
+    Serial.println("Sequence step complete.");
+    delay(20); // put delay so each measurement are stable 
+  }
+  Serial.println("Run sequence complete.");
 }
